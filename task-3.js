464b815ac41a2fa588936ddb5f8f22b474f4095b
GremlenA <pedenkoartem5@gmail.com>
2025-06-01 10:54:54 +0300

    fixed the structure

--- task-3.js
+++
@@ -1,9 +0,0 @@
-function getElementWidth (content,padding,border) 
-{
-       const overallWidth = parseFloat(content) + parseFloat(padding) + parseFloat(border);
-       return overallWidth;
-}
-
-console.log(getElementWidth("50", "8", "4")); // 74
-console.log(getElementWidth("60", "12", "8.5")); // 101
-console.log(getElementWidth("200", "0", "0")); // 200
