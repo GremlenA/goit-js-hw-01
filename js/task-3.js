a8bf7316d95becddf11b80f3b226e4a5f396425a
GremlenA <pedenkoartem5@gmail.com>
2025-06-02 15:26:36 +0300

    fix task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,12 @@
-function getElementWidth (content,padding,border) 
+function getElementWidth(content, padding, border)
 {
-       const overallWidth = parseFloat(content) + parseFloat(padding) + parseFloat(border);
-       return overallWidth;
+    const c = parseFloat(content);
+    const p = parseFloat(padding);
+    const b = parseFloat(border);
+
+    return c + 2 * p + 2 * b;
 }
 
-console.log(getElementWidth("50", "8", "4")); // 74
-console.log(getElementWidth("60", "12", "8.5")); // 101
-console.log(getElementWidth("200", "0", "0")); // 200
+console.log(getElementWidth("50", "8", "4"));
+console.log(getElementWidth("60", "12", "8.5"));
+console.log(getElementWidth("200", "0", "0"));
