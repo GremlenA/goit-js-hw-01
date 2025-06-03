1dd0942cebc1a03aadc91b370456c0d87bb4b116
GremlenA <pedenkoartem5@gmail.com>
2025-06-03 09:18:06 +0300

    Task3 fixed test keys

--- js/task-3.js
+++ js/task-3.js
@@ -7,6 +7,6 @@ function getElementWidth(content, padding, border)
     return c + 2 * p + 2 * b;
 }
 
-console.log(getElementWidth("50", "8", "4"));
-console.log(getElementWidth("60", "12", "8.5"));
-console.log(getElementWidth("200", "0", "0"));
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
